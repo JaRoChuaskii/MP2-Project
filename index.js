e749ab6605b29938b55daace5293493905941e08
JaRoChuaskii <148548328+JaRoChuaskii@users.noreply.github.com>
2023-12-21 21:15:08 +0800

    Add files via upload
    navbar and login/signup form updated

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const formOpenBtn = document.querySelector("#form-open"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form-container"),
+formCloseBtn = document.querySelector(".mb-4"),
+signUpBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login");
+
+function openBtn() {
+    const i = document.getElementById("home");
+
+    if (i.style.display === "block") {
+        i.style.display="none";
+    } else {
+        i.style.display="block";
+        loginBtn.addEventListener("click",(e) =>  {
+            e.preventDefault();
+            formContainer.classList.remove("active");
+        });
+        
+        signUpBtn.addEventListener("click",(e) =>  {
+            e.preventDefault();
+            formContainer.classList.add("active");
+        });
+    }
+}
+
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName());
