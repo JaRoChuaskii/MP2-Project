0cf47aff22137ffb8a3bc342eb98469bd4f85686
JaRoChuaskii <148548328+JaRoChuaskii@users.noreply.github.com>
2024-01-08 22:51:33 +0800

    add to cart functionalities in the menu page

--- index.js
+++ index.js
@@ -1,209 +1,213 @@
 document.addEventListener('DOMContentLoaded', function () {
-	const formOpenBtn = document.querySelector("#form-open"),
-	registerData = document.querySelector("#onRegister"),
-	loginData = document.querySelector("#onLogin"),
-	formContainer = document.querySelector(".form-container"),
-	signUpBtn = document.querySelector("#signup"),
-	logoutBtn = document.querySelector("#logout"),
-	openPanel = document.querySelector("#dropdownMenuButtonLight"),
-	loginBtn = document.querySelector("#login");
+const formOpenBtn = document.querySelector("#form-open"),
+registerData = document.querySelector("#onRegister"),
+loginData = document.querySelector("#onLogin"),
+formContainer = document.querySelector(".form-container"),
+signUpBtn = document.querySelector("#signup"),
+logoutBtn = document.querySelector("#logout"),
+openPanel = document.querySelector("#dropdownMenuButtonLight"),
+openMapModal = document.querySelector("#openMapModal"),
+loginBtn = document.querySelector("#login");
 
-	var password=document.getElementById("floatingPwd");
-	var confirmpwd=document.getElementById("floatingCPassword");
+var password=document.getElementById("floatingPwd");
+var confirmpwd=document.getElementById("floatingCPassword");
 
-	var length = document.getElementById("length");
-	var matchAlert = document.getElementById("matchAlert");
-	var dupAlert = document.getElementById("dupAlert");
+var length = document.getElementById("length");
+var matchAlert = document.getElementById("matchAlert");
+var dupAlert = document.getElementById("dupAlert");
 
-	var f = document.forms["regForm"].elements;
+var f = document.forms["regForm"].elements;
 
-	let loginName, current_user, formattedAddress;
+let loginName, current_user, formattedAddress;
 
 
-	loginBtn.addEventListener("click",(e) =>  {
-		formContainer.classList.remove("active");
-	});
+loginBtn.addEventListener("click",(e) =>  {
+formContainer.classList.remove("active");
+});
 
-	signUpBtn.addEventListener("click",(e) =>  {
-		formContainer.classList.add("active");
-	});
+signUpBtn.addEventListener("click",(e) =>  {
+formContainer.classList.add("active");
+});
 
-	// When the user clicks on the password field, show the message box
-	password.onfocus = function() {
-		document.getElementById("message").style.display = "block";
-	}
+// When the user clicks on the password field, show the message box
+password.onfocus = function() {
+document.getElementById("message").style.display = "block";
+}
 
-	// When the user clicks outside of the password field, hide the message box
-	password.onblur = function() {
-		document.getElementById("message").style.display = "none";
-	}
+// When the user clicks outside of the password field, hide the message box
+password.onblur = function() {
+document.getElementById("message").style.display = "none";
+}
 
-	// When the user starts to type something inside the password field
-	password.onkeyup = function() {
-	// Validate lowercase letters
-		var lowerCaseLetters = /[a-z]/g;
-		if(password.value.match(lowerCaseLetters)) {
-			letter.classList.remove("invalid");
-			letter.classList.add("valid");
-		} else {
-			letter.classList.remove("valid");
-			letter.classList.add("invalid");
-		}
+// When the user starts to type something inside the password field
+password.onkeyup = function() {
+// Validate lowercase letters
+var lowerCaseLetters = /[a-z]/g;
+if(password.value.match(lowerCaseLetters)) {
+letter.classList.remove("invalid");
+letter.classList.add("valid");
+} else {
+letter.classList.remove("valid");
+letter.classList.add("invalid");
+}
 
-	// Validate capital letters
-		var upperCaseLetters = /[A-Z]/g;
-		if(password.value.match(upperCaseLetters)) {
-			capital.classList.remove("invalid");
-			capital.classList.add("valid");
-		} else {
-			capital.classList.remove("valid");
-			capital.classList.add("invalid");
-		}
+// Validate capital letters
+var upperCaseLetters = /[A-Z]/g;
+if(password.value.match(upperCaseLetters)) {
+capital.classList.remove("invalid");
+capital.classList.add("valid");
+} else {
+capital.classList.remove("valid");
+capital.classList.add("invalid");
+}
 
-	// Validate numbers
-		var numbers = /[0-9]/g;
-		if(password.value.match(numbers)) {
-			number.classList.remove("invalid");
-			number.classList.add("valid");
-		} else {
-			number.classList.remove("valid");
-			number.classList.add("invalid");
-		}
+// Validate numbers
+var numbers = /[0-9]/g;
+if(password.value.match(numbers)) {
+number.classList.remove("invalid");
+number.classList.add("valid");
+} else {
+number.classList.remove("valid");
+number.classList.add("invalid");
+}
 
-	// Validate length
-		if(password.value.length >= 8) {
-			length.classList.remove("invalid");
-			length.classList.add("valid");
-		} else {
-			length.classList.remove("valid");
-			length.classList.add("invalid");
-			}
-	}
+// Validate length
+if(password.value.length >= 8) {
+length.classList.remove("invalid");
+length.classList.add("valid");
+} else {
+length.classList.remove("valid");
+length.classList.add("invalid");
+}
+}
 
-	// When the user starts to type something inside the confirm password field
-	confirmpwd.onkeyup = function() {
-		matchAlert.style.display = "block";
+// When the user starts to type something inside the confirm password field
+confirmpwd.onkeyup = function() {
+matchAlert.style.display = "block";
 
-		if (confirmpwd.value.match(password.value)) {
-			matchAlert.innerHTML = "Password is matched";
-			matchAlert.classList.remove("invalid");
-			matchAlert.classList.add("valid");
-			document.getElementById('onRegister').disabled = false;
-		} else {
-			matchAlert.innerHTML = "Password is not matched";
-			matchAlert.classList.remove("valid");
-			matchAlert.classList.add("invalid");
-		for (var i = 0; i < f.length; i++) {
-			if (f[i].value.length == 0) cansubmit = false;
-			document.getElementById('onRegister').disabled = true;
-			}
-		}
-	}
+if (confirmpwd.value.match(password.value)) {
+matchAlert.innerHTML = "Password is matched";
+matchAlert.classList.remove("invalid");
+matchAlert.classList.add("valid");
+document.getElementById('onRegister').disabled = false;
+} else {
+matchAlert.innerHTML = "Password is not matched";
+matchAlert.classList.remove("valid");
+matchAlert.classList.add("invalid");
+for (var i = 0; i < f.length; i++) {
+if (f[i].value.length == 0) cansubmit = false;
+document.getElementById('onRegister').disabled = true;
+}
+}
+}
 
-	registerData.addEventListener('click', function (event) {
-		
-		let fname, lname, num, email, pass, address;
+registerData.addEventListener('click', function (event) {
 
-		fname=document.getElementById("floatingFname").value;
-		lname=document.getElementById("floatingLname").value;
-		num=document.getElementById("floatingNumber").value;
-		email=document.getElementById("floatingEmail").value;
-		pass=document.getElementById("floatingPwd").value;
-		address=null;
+let fname, lname, num, email, pass, address;
 
-		let user_records = new Array();
-		user_records = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
-		if (user_records.some((v) => {
-			return v.email==email
-		})) {
-			dupAlert.innerHTML = "Email already exist. Please login!";
-			dupAlert.style.display = "block";
-		}else {
-		user_records.push({
-			"fname":fname,
-			"lname":lname,
-			"num":num,
-			"email":email,
-			"pass":pass,
-			"address":address
-		})
-		localStorage.setItem("users", JSON.stringify(user_records));
-		window.location.reload();
-		}
-	});
+fname=document.getElementById("floatingFname").value;
+lname=document.getElementById("floatingLname").value;
+num=document.getElementById("floatingNumber").value;
+email=document.getElementById("floatingEmail").value;
+pass=document.getElementById("floatingPwd").value;
+address=null;
 
-	loginData.addEventListener('click', function (event) {
-		
+let user_records = new Array();
+user_records = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+if (user_records.some((v) => {
+return v.email==email
+})) {
+dupAlert.innerHTML = "Email already exist. Please login!";
+dupAlert.style.display = "block";
+}else {
+user_records.push({
+"fname":fname,
+"lname":lname,
+"num":num,
+"email":email,
+"pass":pass,
+"address":address
+})
+localStorage.setItem("users", JSON.stringify(user_records));
+window.location.reload();
+}
+});
 
-		let email, passwords;
-		email=document.getElementById("userEmail").value;
-		passwords=document.getElementById("userPassword").value;
+loginData.addEventListener('click', function (event) {
 
-		let login_records = new Array();
-		login_records = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
-		if (login_records.some((v) => {
-			return v.email==email && v.pass==passwords
-			})) {
-			current_user=login_records.filter((v) => {
-			return v.email==email && v.pass==passwords 
-			})[0]
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(onSuccess, onError);
-			}else{
-				alert("Browser doesn't support Geolocation API.");
-			}
-		}else {
-			alert("you are not logged in!");
-		}
-	});
 
-	function onSuccess(position) {
-	let {latitude, longitude} = position.coords;
-	//https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY
-		fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&key=AIzaSyCF-np2mc0J1ur6NCQTQ7_YMNlSyD7VZLU')
-		.then(response => response.json()).then(result => {
-		formattedAddress = result.results[0].formatted_address;
-		loginName=current_user.fname;
-		current_user.address = formattedAddress;
-		let loginLoc = current_user.address;
-		localStorage.setItem("loggedIn", JSON.stringify(current_user));
-		showLoggedInState(loginName, loginLoc);
-		});
-	}
+let email, passwords;
+email=document.getElementById("userEmail").value;
+passwords=document.getElementById("userPassword").value;
 
-	function onError(error) {
-		if(error.code==1){
-			alert("Request Denied!");
-		}else if(error.code==2){
-			alert("Location is not available!");
-		}else{
-			alert("Something went wrong!");
-		}
-	}
+let login_records = new Array();
+login_records = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+if (login_records.some((v) => {
+return v.email==email && v.pass==passwords
+})) {
+current_user=login_records.filter((v) => {
+return v.email==email && v.pass==passwords 
+})[0]
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}else{
+alert("Browser doesn't support Geolocation API.");
+}
+}else {
+alert("you are not logged in!");
+}
+});
 
-	// Check if the user is already logged in
-	let storedUsername = new Array();
-	storedUsername = JSON.parse(localStorage.getItem("loggedIn"))?JSON.parse(localStorage.getItem("loggedIn")):[]
-	if (storedUsername.fname){
-		const loginName=storedUsername.fname;
-		const loginLoc=storedUsername.address;
-		showLoggedInState(loginName, loginLoc);
-	}
+function onSuccess(position) {
+let {latitude, longitude} = position.coords;
+//https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY
+fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&key=AIzaSyCF-np2mc0J1ur6NCQTQ7_YMNlSyD7VZLU')
+.then(response => response.json()).then(result => {
+formattedAddress = result.results[0].formatted_address;
+loginName=current_user.fname;
+current_user.address = formattedAddress;
+let loginLoc = current_user.address;
+localStorage.setItem("loggedIn", JSON.stringify(current_user));
+showLoggedInState(loginName, loginLoc);
+});
+}
 
-	// Helper functions
-	function showLoggedInState(loginName, loginLoc) {
-		console.log(loginLoc);
-		formContainer.style.display = "none";
-		formOpenBtn.style.display = "none";
-		openPanel.style.display = "block";
-		openPanel.innerHTML = "Hi " + loginName;
-	}
+function onError(error) {
+if(error.code==1){
+alert("Request Denied!");
+}else if(error.code==2){
+alert("Location is not available!");
+}else{
+alert("Something went wrong!");
+}
+}
 
-	// Logout functions
-	logoutBtn.addEventListener('click', function() {
-		localStorage.removeItem("loggedIn");
-		window.location.reload();
-	});
+// Check if the user is already logged in
+let storedUsername = new Array();
+storedUsername = JSON.parse(localStorage.getItem("loggedIn"))?JSON.parse(localStorage.getItem("loggedIn")):[]
+if (storedUsername.fname){
+const loginName=storedUsername.fname;
+const loginLoc=storedUsername.address;
+showLoggedInState(loginName, loginLoc);
+}
+
+// Helper functions
+function showLoggedInState(loginName, loginLoc) {
+console.log(loginLoc);
+formContainer.style.display = "none";
+formOpenBtn.style.display = "none";
+openPanel.style.display = "block";
+openPanel.innerHTML = "Hi " + loginName;
+openMapModal.style.display = "block";
+openMapModal.innerHTML = "Your current location: " + loginLoc;
+document.getElementById("addressInput").setAttribute("value",loginLoc);
+}
+
+// Logout functions
+logoutBtn.addEventListener('click', function() {
+localStorage.removeItem("loggedIn");
+window.location.href = "index.html";
+});
 
 });
 
@@ -212,36 +216,118 @@ let map;
 var locate = document.getElementById("addressInput");
 
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-	center: {lat: 7.088079785342349, lng: 125.61169773054502},
-	zoom: 15
-	});
+map = new google.maps.Map(document.getElementById('map'), {
+center: {lat: 7.088079785342349, lng: 125.61169773054502},
+zoom: 15
+});
 }
 
 locate.onkeyup = function() {
 
-	const address = document.getElementById('addressInput').value;
-	const geocoder = new google.maps.Geocoder();
+const address = document.getElementById('addressInput').value;
+const geocoder = new google.maps.Geocoder();
 
-	geocoder.geocode({'address': address}, function(results, status) {
-		if (status === 'OK') {
-			map.setCenter(results[0].geometry.location);
+geocoder.geocode({'address': address}, function(results, status) {
+if (status === 'OK') {
+map.setCenter(results[0].geometry.location);
 
-			const marker = new google.maps.Marker({
-			map: map,
-			position: results[0].geometry.location,
-			zoom: 15
-			});
+const marker = new google.maps.Marker({
+map: map,
+position: results[0].geometry.location,
+zoom: 15
+});
 
-			// You can customize the info window content here
-			const infowindow = new google.maps.InfoWindow({
-			content: '<strong>' + results[0].formatted_address + '</strong>'
-			});
+// You can customize the info window content here
+const infowindow = new google.maps.InfoWindow({
+content: '<strong>' + results[0].formatted_address + '</strong>'
+});
 
-			marker.addListener('click', function() {
-			infowindow.open(map, marker);
-			});
-		} else {
-		}
-	});
+marker.addListener('click', function() {
+infowindow.open(map, marker);
+});
+} else {
+}
+});
+}
+
+const product = [
+{
+id: 0,
+image: 'cars/accent.png',
+categories: 'coffee',
+title: 'Coffee 1',
+price: 200,
+},
+{
+id: 1,
+image: 'cars/miragehatch.png',
+title: 'Coffee 2',
+price: 360,
+},
+{
+id: 2,
+image: 'cars/mazda.png',
+title: 'Coffee 3',
+price: 230,
+},
+{
+id: 3,
+image: 'cars/montero.png',
+title: 'Coffee 4',
+price: 700,
+}
+];
+const categories = [...new Set(product.map((item)=>
+{return item}))]
+let i=0;
+document.getElementById('coffee').innerHTML = categories.map((item)=>
+{
+var {image, title, price} = item;
+return(
+`<div class='card'>
+<img class='images' src=${image}></img>
+<div class='card-body'>
+<h5>${title}</h5>
+<p>Php ${price}.00</p>`+
+"<button class='btn btn-primary' onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+`</div>
+</div>`
+)
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+cart.push({...categories[a]});
+displaycart();
+}
+function delElement(a){
+cart.splice(a, 1);
+displaycart();
+}
+
+function displaycart(){
+let j = 0, total=0;
+document.getElementById("count").innerHTML=cart.length;
+if(cart.length==0){
+document.getElementById('cartItem').innerHTML = "Your cart is empty";
+document.getElementById("total").innerHTML = "$ "+0+".00";
+}
+else{
+document.getElementById("cartItem").innerHTML = cart.map((items)=>
+{
+var {image, title, price} = items;
+total=total+price;
+document.getElementById("total").innerHTML = "Php "+total+".00";
+return(
+`<div class='cart-item'>
+<div class='row-img'>
+	<img class='rowimg' src=${image}>
+</div>
+<p style='font-size:12px;'>${title}</p>
+<h2 style='font-size: 15px;'>Php ${price}.00</h2>`+
+"<span class='material-symbols-outlined' onclick='delElement("+ (j++) +")'>delete</span></div>"
+);
+}).join('');
+}
 }
